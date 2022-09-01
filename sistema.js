ea3488f24f0135174d7d11158786bbf9c316172b
Djênifer Gurskas <gurskasdjenifer@gmail.com>
2022-08-31 23:46:37 -0300

    commit
    commit

--- sistema.js
+++ sistema.js
@@ -1,7 +1,7 @@
-const seatArray = [];
-const seatWidth = 4;
-const seatHeigth = 12;
-const seatCount = 46;
+const seatArray = []
+const seatWidth = 4
+const seatHeigth = 12
+const seatCount = 46
 
 function start(){
 	createTableStructure()
@@ -9,13 +9,14 @@ function start(){
 
 	// occupy 30% of seats
 	occupySeats()
+
+	//
+	populateLists()
 }
 
 function createTableStructure(){
-
-	
 	for(let i = 0; i <= seatCount; i++){
-		seatArray.push(new seat(i));
+		seatArray.push(new seat(i))
 	}
 }
 
@@ -30,14 +31,21 @@ function renderSeats(){
 
 			const pixelIndex = column + (seatWidth * row)
 			if(pixelIndex < seatCount){
-				let seatRoute = seatArray.find(x => x.index == pixelIndex).route
-				if(!seatRoute)
-					seatRoute = ''
+				
+				html += `<div class="pixel-index">Assento ${pixelIndex+1}</div>`
 
-				html += `<div class="pixel-index">${pixelIndex+1}</div>`
-				html += seatRoute
+				let s = seatArray.find(x => x.index == pixelIndex)
+
+				html += '<div class="time">'
+				html += `<div class="time-available">${times.filter(x=>x !== s.time)}</div>`
+				if(s.time)
+					html += `<div class="time-occupied">${s.time}</div>`
+				html += '</div>'
+
+				if(s.route)
+					html += s.route
 			}
-			
+
 			html += '</td>'
 		}
 
@@ -53,27 +61,58 @@ function occupySeats(){
 	const seatsToOccupy = Math.round(seatCount * 0.3)
 	for(let i = 0; i < seatsToOccupy; i++){
 		let s = seatArray.find(x => x.index == i)
-
+		let route
+		
 		if(i < seatsToOccupy / 2)
-			s.route = 'Porto Alegre -> Florianópolis'
+			route = routes[0]
 		else
-			s.route = 'Porto Alegre -> Criciúma'
+			route = routes[1]
+		
+		s.time = route.time[0]
+		s.route = route.route
 	}
 
 	renderSeats()
 }
 
+function occupySeat(index){
+	let s = seatArray.find(x => x.index == index)
+	s.route = routes[0].route
+	s.time = routes[0].time[0]
 
-function seat(index,route,time){
+	renderSeats()
+	populateLists()
+}
+
+function populateLists(){
+	let html = ''
+
+	for(let i = 0; i < seatArray.length; i++){
+		let s = seatArray.find(x => x.index == i)
+		if(!s.time)
+			html += `<option value="${i}">Assento ${s.index+1}</option>`
+	}
+
+	document.getElementById('bookSeat').innerHTML = html;
+}
+
+
+function seat(index, route, time){
 	this.index = index
 	this.route = route
 	this.time = time
-	this.setTime = function (time){
-		this.time = time
-	}
-	this.setRoute = function (route){
-		this.route = route;
-	}
 }
+const routes = [{
+	route: "Porto Alegre -> Florianópolis",
+	time: ["6h","16h"]},
+{
+	route: "Porto Alegre -> Criciúma",
+	time: ["6h","16h"]},
+{
+	route: "Criciúma -> Florianópolis",
+	time: ["10h","20h"]
+}]
+const times = ["6h", "10h", "16h", "20h"]
+
 
 start()
